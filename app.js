0912d0222577ff4c0aaef6f90a311672e7ce4ecb
lnguyen-cpi <nblam1994@gmail.com>
2023-04-11 22:17:15 -0700

    Init project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+var server = app.listen(3000, () => {
+    console.log("Server is running at " + server.port);
+})
+
