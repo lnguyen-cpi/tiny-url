341945f896a581766054fa4be79861c83ae7655e
lnguyen-cpi <nblam1994@gmail.com>
2023-04-16 23:13:16 -0700

    Implement post/get logic

--- routes/v1/shortenUrl.js
+++ routes/v1/shortenUrl.js
@@ -1,12 +1,37 @@
 const express = require("express");
+const {urls} = require("../../models");
 const router = express.Router();
 
-router.post("/data/shortenUrl", (req, res) => {
-    res.send("Get URL");
+router.post("/data/shortenUrl", async (req, res) => {
+    const {longUrl} = req.body;
+
+    try {
+        const url = await urls.create({
+            shortUrl: (Math.random() + 1).toString(36).substring(7),
+            longUrl: longUrl,
+        });
+        return res.json(url);
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
+   
 })
 
-router.get("/:shortUrl", (req, res) => {
-    res.send(req.params.shortUrl);
+router.get("/:shortUrl", async (req, res) => {
+    const {shortUrl} = req.params;
+
+    try {
+        const longUrl = await urls.findOne({
+            where: {shortUrl: shortUrl}
+        });
+        return res.json(longUrl);
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Something went wrong' });
+    }
 })
 
 module.exports = router;
